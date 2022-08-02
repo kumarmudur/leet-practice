444b795df464a098c022ef3657636fa34ab86e9c
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-08-02 06:19:07 +0530

    [Easy][Array] contains duplicate example

---
+++ easy/containsDuplicate.js
@@ -0,0 +1,19 @@
+// Array and Hash map
+
+// Input: nums = [1,2,3,1] : true
+// Input: nums = [1,2,3,4] : false
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]: true
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+const containsDuplicate = function(nums) {
+    const hash = {};
+    for (const num of nums) {
+        if (!hash[num]) hash[num] = true;
+        else return true;
+    }
+    return false;
+};
