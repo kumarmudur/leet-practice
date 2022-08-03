3e826d0b0ce4ce55c8e57cd3d2b61e451f8fa906
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-08-03 05:43:04 +0530

    [Easy][Math] palindrome number check solution

--- easy/palindromeNumber.js
+++ easy/palindromeNumber.js
@@ -33,4 +33,16 @@ const isPalindrome1 = function(x) {
     return true;
 }
 
-console.log(isPalindrome(-121));
+// time: O(log(n) | space: O(1)
+const isPalindrome2 = function(x) {
+    if (x < 0 ) return false;
+    let num = x;
+    let res = 0;
+    while(num !== 0) {
+        res = (res * 10) + (num % 10);
+        num = Math.floor(num / 10);
+    }
+    return res === x;
+}
+
+console.log(isPalindrome(121));
