d70dbfe51d3a1e1f882bb15479fb01f66f21411b
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-08-03 05:31:57 +0530

    [Easy][Math] palindrome number check

--- easy/palindromeNumber.js
+++ easy/palindromeNumber.js
@@ -19,7 +19,18 @@ const isPalindrome = function(x) {
     return revChar === num;
 };
 
-
-
+// time: O(log(n) | space: O(1)
+const isPalindrome1 = function(x) {
+    let str = '' + x;
+    let left = 0;
+    let right = str.length - 1;
+    console.log(str);
+    while (left < right) {
+        if (str[left] !== str[right]) return false;
+        left++;
+        right--;
+    }
+    return true;
+}
 
 console.log(isPalindrome(-121));
