488041255bbf7f4207a461681986b89285a6ead2
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-08-05 04:40:12 +0530

    [Array][Math] plus one example

---
+++ easy/plusOne.js
@@ -0,0 +1,26 @@
+// plus one 
+// Topic: Array, Math
+// Input: [1,2,3] => [1,2,4]
+// Input: [4,3,2,1] => [4,3,2,2]
+// Output: [9,9] => [1,0,0]
+
+
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+const plusOne = function(digits) {
+   let i = digits.length - 1;
+   let val = 0;
+   let carry = 1;
+   while(digits[i] && carry) {
+    val = digits[i] + carry;
+    carry = Math.floor(val / 10);
+    digits[i] = val % 10;
+    i--;
+   }
+   if (carry) digits.unshift(carry);
+   return digits;
+};
+
+console.log(plusOne([9, 9]));
