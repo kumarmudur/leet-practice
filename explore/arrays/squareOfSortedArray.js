991c2721b0564d3dae661dee2cf2a4dd31ad2c1b
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-08-10 07:25:32 +0530

    [Explore][Array] square of sorted arrays example solution 2

--- explore/arrays/squareOfSortedArray.js
+++ explore/arrays/squareOfSortedArray.js
@@ -1,4 +1,6 @@
-
+Topic: Arrays
+// Input: nums = [-4,-1,0,3,10] => [0,1,9,16,100]
+// Input: nums = [-7,-3,2,3,11] => [4,9,9,49,121]
 
 /**
  * @param {number[]} nums
@@ -11,6 +13,16 @@ const sortedSquares = function(nums) {
         nums[i] = squareOfNumber(nums[i]);
     }
     return nums.sort((a, b) => a - b);
+};
+
+// Solution 2
+// time: O(n) | space: O(n)
+const sortedSquares1 = function(nums) {
+    let results = []
+    for (let i = 0; i < nums.length; i++) {
+        results.push(squareOfNumber(nums[i]));
+    }
+    return results.sort((a, b) => a - b);
 };
 
 const squareOfNumber = num => num * num;
