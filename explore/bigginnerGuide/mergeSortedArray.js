ba77027d791883e7776b4de90dfb86a8c47311a9
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-08-13 19:09:22 +0530

    [School][Arrays] merge two arrays solution 2

--- explore/bigginnerGuide/mergeSortedArray.js
+++ explore/bigginnerGuide/mergeSortedArray.js
@@ -1,3 +1,8 @@
+// Topic: Array;
+// Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3 => [1,2,2,3,5,6]
+// Input: nums1 = [1], m = 1, nums2 = [], n = 0 => [1]
+// Input: nums1 = [0], m = 0, nums2 = [1], n = 1 => [1]
+
 /**
  * @param {number[]} nums1
  * @param {number} m
@@ -19,5 +24,26 @@ const merge = function(nums1, m, nums2, n) {
     nums1.sort((a, b) => a - b);
     return nums1;
 };
+
+// Solution 2
+const merge1 = function(nums1, m, nums2, n) {
+    let result = [];
+    let merge1 = [];
+    let merge2 = [];
+
+    for (let i = 0; i < m; i++) {
+        if (nums1[i] !== 0) {
+            merge1.push(nums1[i]);
+        }
+    }
+
+    for (let i = 0; i < n; i++) {
+        if (nums2[i] !== 0) {
+            merge2.push(nums2[i]);
+        }
+    }
+    result = [...merge1, ...merge2];
+    return sortNumber(result);
+}
 
 console.log(merge([1,2,3,0,0,0], 3, [2,5,6], 3));
