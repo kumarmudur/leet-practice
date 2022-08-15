a0c3e7e3cce62db07a66ee5dafb4fca458aa5b74
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-08-15 19:51:37 +0530

    [136][Easy][Array] single number example solution 2

--- easy/singleNumber.js
+++ easy/singleNumber.js
@@ -14,6 +14,21 @@ const singleNumber = function(nums) {
     return output;
 };
 
+// Solution 2
+// time: O(n) | space: O(n)
+const singleNumber1 = function(nums) {
+    if(nums.length === 0) return false;
+    if(nums.length === 1) return nums[0];
+
+    let counter = {};
+    for (let i = 0; i < nums.length; i++) {
+        counter[nums[i]] ? counter[nums[i]] += 1 : counter[nums[i]] = 1;
+    }
+
+    for (let key in counter) {
+        if (counter[key] === 1) return key;
+    }
+}
 
 console.log(singleNumber([4,1,2,1,2]));
 
