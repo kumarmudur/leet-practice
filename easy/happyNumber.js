f9bb62963ba338b14f0bbbaf4343d47f1b72c119
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-08-17 06:14:22 +0530

    [202][Hash Table][Math][Two Pointers] happy number example

---
+++ easy/happyNumber.js
@@ -0,0 +1,30 @@
+// Topic: Hash Table, Math, Two Pointers
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+
+// Solution 1
+// time: O(n) | space: O(1)
+const isHappy = function(n) {
+    let map = {};
+    let tmp = 0;
+
+    if (n < 1) return false;
+
+    while (n !== 1 && !map[n]) {
+        map[n] = true;
+        tmp = 0;
+
+        while (n > 0) {
+            console.log('line 19', tmp);
+            tmp += Math.pow(n % 10, 2);
+            n = Math.floor(n / 10);
+        }
+        n = tmp;
+    }
+    return n === 1;
+};
+
+console.log(isHappy(19));
