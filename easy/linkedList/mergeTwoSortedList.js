566184d64e05574ec989a294c36e7011b5c34039
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-08-18 10:21:46 +0530

    [Easy][Linked List] merge two sorted list

---
+++ easy/linkedList/mergeTwoSortedList.js
@@ -0,0 +1,44 @@
+// Topic: Linked List, Recursion
+// Input: 1->2->4, 1->3->4 => 1->1->2->3->4->4
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+
+function ListNode(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+}
+
+const mergeTwoLists = function(list1, list2) {
+    let head = new ListNode(0);
+    let now = head;
+
+    let p1 = list1;
+    let p2 = list2;
+
+    while (p1 || p2) {
+        console.log('p1.val', p1.val);
+        console.log('p2.val', p2.val);
+        if (p1 === null || (p2 !== null && p2.val < p1.val)) {
+            now.next = p2;
+            p2 = p2.next;
+        } else {
+            now.next = p1;
+            p1 = p1.next;
+        }
+        now = now.next;
+    }
+    return head.next;
+};
+
+mergeTwoLists([1,2,4], [1,3,4]);
