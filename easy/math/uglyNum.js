3afac47473f82564d7eec61dec5aac7c868c6d30
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-08-20 06:19:17 +0530

    [263][Math] ugly number example solution 2

--- easy/math/uglyNum.js
+++ easy/math/uglyNum.js
@@ -23,4 +23,17 @@ const isUgly = function(n) {
     return n === 1;
 };
 
-console.log(isUgly(6));
+// Solution 2
+const isUgly2 = function(n) {
+    if (n <= 0) return false;
+
+    while (n !== 1) {
+        if (n % 2 === 0) n /= 2;
+        else if (n % 3 === 0) n /= 3;
+        else if (n % 5 === 0) n /= 5;
+        else return false;
+    }
+    return true;
+}
+
+console.log(isUgly2(14));
