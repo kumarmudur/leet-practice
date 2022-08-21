428a2c2c62725c5fd90cd61ccda15f6c8ccf9cd1
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-08-21 07:07:23 +0530

    [258][JS][Easy][Math][Simulation][Number Theory] add digits example

---
+++ easy/simulation/addDigits.js
@@ -0,0 +1,37 @@
+// Topic: Math, Simulation, Number Theory
+
+// Input: num = 38
+// Output: 2
+// Explanation: The process is
+// 38 --> 3 + 8 --> 11
+// 11 --> 1 + 1 --> 2 
+// Since 2 has only one digit, return it.
+
+// Input: num = 0
+// Output: 0
+
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+const addDigits = function(num) {
+  let newStr = num.toString().split('');
+  if (newStr.length === 1) return newStr[0];
+
+   function addDigitsValues(n) {
+        let result = 0;
+        
+        for (let i = 0; i < n.length; i++) {
+            result += Number(n[i]);
+        }
+        if (result.toString().split('').length === 1) { 
+            return result; 
+        } else {
+          return addDigitsValues(result.toString().split(''));
+        }
+   }
+  return addDigitsValues(newStr);
+};
+
+console.log(addDigits(89));
