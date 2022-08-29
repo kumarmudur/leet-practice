1fd06b9fe18089f8b612040c32d10ac53ab2e935
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-08-29 10:14:01 +0530

    [509][JS][Easy][Math][Top Down DP] fibonacci number example solution 2

--- explore/recursion/fib.js
+++ explore/recursion/fib.js
@@ -7,7 +7,23 @@
 
 // Solution 1
 // time: O(2^n) | space: O(n)
-const fib = function(n) {
+const fib = (n) => {
    if (n < 2) return n;
    return fib(n - 1) + fib(n - 2);
  };
+
+ // Solution 2
+ // Top-down Dynamic Programming with Memoization
+const fib1 = (n) => {
+    const memoize = [];
+
+    function calculateFib(n) {
+        if (n < 2) return n;
+
+        if (memoize[n]) return memoize[n];
+
+        memoize[n] = calculateFib(n - 1) + calculateFib(n - 2);
+        return memoize[n];
+    }
+    return calculateFib(n);
+}
