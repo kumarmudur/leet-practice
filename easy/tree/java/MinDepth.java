5c94e2d0c41a75c80a3b2540ff51f939e4183d98
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-09-11 08:12:30 +0530

    [111][Java][Easy][Tree][DFS][BFS][BT] min depth of tree example solution

---
+++ easy/tree/java/MinDepth.java
@@ -0,0 +1,31 @@
+// Topic: Tree, DFS, BFS, BT
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int leftDepth = minDepth(root.left);
+        int rightDepth = minDepth(root.right);
+        if (leftDepth == 0 || rightDepth == 0) {
+            return Math.max(leftDepth, rightDepth) + 1;
+        } else {
+            return Math.min(leftDepth, rightDepth) + 1;
+        }
+    }
+}
