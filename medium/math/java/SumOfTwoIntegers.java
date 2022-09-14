042320f6d9893cd6dbc259ffd745daea1d139e31
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-09-14 06:06:54 +0530

    [371][JAVA][Medium][Math][Bit Manipulation] sum of two integers example solution

---
+++ medium/math/java/SumOfTwoIntegers.java
@@ -0,0 +1,14 @@
+// Topic: Math, Bit Manipulation
+
+package medium.math.java;
+
+public class SumOfTwoIntegers {
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int temp = (a & b) << 1;
+            a = a ^ b;
+            b = temp;
+        }
+        return a;
+    }
+}
