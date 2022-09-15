01f34bdb7c437f444f2941389d5973d78e187c12
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-09-15 07:40:56 +0530

    [141][[JAVA][Easy][LinkedList][Hash Table][Two Pointers] linked list cycle detection

---
+++ easy/linkedList/java/linkedListCycle.java
@@ -0,0 +1,30 @@
+package easy.linkedList.java;
+
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class linkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null)
+            return false;
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast)
+                return true;
+        }
+        return false;
+    }
+}
