c30a95a6e7372b028a3edd4081132b9a6939baa2
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-09-16 09:06:09 +0530

    [56][JAVA][Medium][Arrays] merge intervals example solution

---
+++ medium/array/java/mergeIntervals.java
@@ -0,0 +1,33 @@
+package medium.array.java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+class mergeIntervals {
+    public int[][] merge(int[][] intervals) {
+        List<int[]> result = new ArrayList<>();
+
+        if (intervals.length == 0 || intervals == null) {
+            return result.toArray(new int[0][]);
+        }
+
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+
+        for (int[] i : intervals) {
+            if (i[0] <= end) {
+                end = Math.max(end, i[1]);
+            } else {
+                result.add(new int[] { start, end });
+                start = i[0];
+                end = i[1];
+            }
+        }
+        result.add(new int[] { start, end });
+        return result.toArray(new int[0][]);
+    }
+}
