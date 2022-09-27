3ef4c444150a5fb635723df6a600b45012222203
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-09-27 06:38:39 +0530

    [350][JS][Easy][Arrays][TP][Hash Table][BS][Sorting] intersection example solution

--- easy/arrays/js/intersection2.js
+++ easy/arrays/js/intersection2.js
@@ -5,9 +5,11 @@ function intersection(nums1, nums2) {
     nums1.sort((a, b) => a - b);
     nums2.sort((a, b) => a - b);
     
+    let len1 = nums1.length;
+    let len2 = nums2.length;
     let i = 0, j = 0, result = [];
     
-    while (i < nums1.length && j < nums2.length) {
+    while (i < len1 && j < len2) {
         if (nums1[i] < nums2[j]) i++;
         else if (nums1[i] > nums2[j]) j++;
         else {
@@ -19,4 +21,29 @@ function intersection(nums1, nums2) {
     return result;
 }
 
-intersection([1,2,2,1], [2,2]);
+// time: O(n) | space: O(n) 
+function intersection2(nums1, nums2) {
+    nums1.sort((a, b) => a - b);
+    nums2.sort((a, b) => a - b);
+
+    let len1 = nums1.length;
+    let len2 = nums2.length;
+    let i = 0, j = 0;
+    const set = new Set();
+    const result = [];
+
+    while(i < len1 && j < len2) {
+        if (nums1[i] < nums2[j]) i++;
+        else if (nums1[i] > nums2[j]) j++;
+        else {
+            set.add(nums1[i]);
+            i++;
+            j++;
+        }
+    }
+
+    for (const item of set) result.push(item);
+    return result;
+}
+
+intersection2([1,2,2,1], [2,2]);
